8eab18a10a655f7fd04bf3561e45dffb34966fea
Masakazu Ohtsuka <o.masakazu@gmail.com>
2017-11-16 16:00:45 +0900

    Some more comments for ease of mind

--- redigostore/redigostore.go
+++ redigostore/redigostore.go
@@ -21,8 +21,16 @@ func (s store) Get(key string, expires time.Duration) (int64, error) {
 	defer conn.Close()
 
 	conn.Send("MULTI")
+
+	// https://redis.io/commands/incr
+	// If the key does not exist, it is set to 0 before performing the operation
 	conn.Send("INCR", key)
+
+	// https://redis.io/commands/expire
+	// For instance, incrementing the value of a key with INCR, ...
+	// are all operations that will leave the timeout untouched.
 	conn.Send("EXPIRE", key, int(expires.Seconds()))
+
 	v, err := redis.Int64s(conn.Do("EXEC"))
 	if err != nil {
 		return 0, err
